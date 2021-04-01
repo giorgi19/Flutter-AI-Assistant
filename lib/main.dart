5c151b549b2c1a013a3ffd23af73f7667d95021a
Girogi <nadiradze19@gmail.com>
2021-04-01 10:07:33 +0400

    Assistant

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:assistant/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+
+SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Assistant',
+      theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily
+
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+
+    );
+  }
+}
